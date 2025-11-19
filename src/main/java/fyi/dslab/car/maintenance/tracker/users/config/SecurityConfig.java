e2d652f94fbb4e75c759eb8919dc6d73384b85d3
Dmytro Shvechikov <quitereality@gmail.com>
2025-11-19 10:10:53 +0100

    disable csrf back

--- src/main/java/fyi/dslab/car/maintenance/tracker/users/config/SecurityConfig.java
+++ src/main/java/fyi/dslab/car/maintenance/tracker/users/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package fyi.dslab.car.maintenance.tracker.users.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,7 +12,8 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/users",
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/users",
                         "/auth-code",
                         "/login",
                         "/swagger-ui.html",
